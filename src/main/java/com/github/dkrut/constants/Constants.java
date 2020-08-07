349a432b1ae141916dd079f38ade01fa2e1dce86
dkrut <dd.krutikov@gmail.com>
2020-08-07 22:22:59 +0300

    missed constants

---
+++ src/main/java/com/github/dkrut/constants/Constants.java
@@ -0,0 +1,15 @@
+package com.github.dkrut.constants;
+
+public class Constants {
+    public static class ServerName {
+        public static String GOOGLE_PlACES_SERVER = "https://maps.googleapis.com/maps/";
+    }
+
+    public static class Path {
+        public static String GOOGLE_PlACE_PATH = "api/place/";
+    }
+
+    public static class Endpoint {
+        public static String GOOGLE_PlACE_ENDPOINT_SEARCH = "findplacefromtext/json/";
+    }
+}
